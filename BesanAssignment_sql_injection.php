a6c16210ee325facdc26c30bb9d7290f4af3be68
Besan-Ayman-AbuZubaida <hebaa2510@gmail.com>
2023-04-19 01:07:08 +0300

    last commit add htmlspecialcharaters in <input type="text" class="form-control" name="userName" value="<?php echo htmlspecialchars($userName, ENT_QUOTES); ?>">

--- BesanAssignment_sql_injection.php
+++ BesanAssignment_sql_injection.php
@@ -102,11 +102,11 @@ else{ //count($errors)== 0
 <form action="" method="post" enctype="multipart/form-data">
 <div class="mb-3">
     <label for="exampleInputUserName" class="form-label">User Name</label>
-    <input type="text" class="form-control" name="userName" value=<?php echo $userName?> >
+    <input type="text" class="form-control" name="userName" value=<?php echo htmlspecialchars($userName, ENT_QUOTES); ?> >
   </div>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" value=<?php echo $email?>>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" value=<?php echo htmlspecialchars($email, ENT_QUOTES); ?>> 
     <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
   </div>
   <div class="mb-3">
